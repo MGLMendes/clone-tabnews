2620989b8fd1710a887695d81a57e69365c535e4
MGLMendes <miguelmendes203@gmail.com>
2025-12-02 16:49:34 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>RolRolzinha, meu site está no ar!! Fique impactada pls</h1>
+  return <h1>RolRolzinha, meu site está no ar!! Fique impactada pls</h1>;
 }
 
 export default Home;
