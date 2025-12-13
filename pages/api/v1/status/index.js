22bc58b6a50ffd6255aab47dc1e325663407693d
MGLMendes <miguelmendes203@gmail.com>
2025-12-13 14:06:30 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
+  const updatedAt = new Date().toISOString();
+
+  const resultShowVersion = await database.query("SHOW server_version;");
+  const postgresVersion = resultShowVersion.rows[0].server_version;
+
+  const resultMaxConnections = await database.query("SHOW max_connections;");
+  const postgresMaxConnections = resultMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const resultOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const postgresConnections = resultOpenedConnections.rows[0].count;
+
   response.status(200).json({
-    chave: "Deu bão dnv",
+    updated_at: updatedAt,
+    dependencies: {
+      version: postgresVersion,
+      max_connections: parseInt(postgresMaxConnections),
+      opened_connections: postgresConnections,
+    },
   });
 }
 
